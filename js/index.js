59e744b34488bf1e6c2fe9d9b832ae515e847ea9
Cynthia <cynthiatong16@gmail.com>
2017-05-28 17:13:25 +0400

    update responsive transition

--- js/index.js
+++ js/index.js
@@ -2,51 +2,28 @@ var main = document.getElementById("main");
 var navArea = document.querySelector("#nav");
 var navBar = document.querySelector(".nav-bar");
 var navContent = document.querySelector("header");
-var status = "none";
-var opacity = "1";
-var bg = "none";
 
 function adjustNav() {
     if ($(window).width() < 950) {
 
-        navContent.style.display = status;
-
-        $(".nav-bar").click(function() {
-            status = (status == "block")? "none" : "block";
-            navContent.style.display = status;
-
-            bg = (bg == "none")? "#e6e6e6" : "none";
-
-            // opacity = (opacity == "1")? "0.9" : "1";
-            // main.style.opacity = opacity;
-            // navContent.style.opacity = "1";
-
-            navArea.style.background = bg;
-        });
 
         $(".nav-bar").hover(function() {
-            navContent.style.display = "block";
-            // main.style.opacity = "0.9";
-            navArea.style.background = "#e6e6e6";
+            navArea.className = "nav-transform";
+            navArea.style.transform =  "translateX(15em)";
         });
 
         $("#nav").mouseover(function() {
-            navContent.style.display = "block";
-            // main.style.opacity = "0.9";
-            navArea.style.background = "#e6e6e6";
+            navArea.className = "nav-transform";
+            navArea.style.transform =  "translateX(15em)";
         });
 
         $("#nav").mouseout(function() {
-            navContent.style.display = "none";
-            // main.style.opacity = "1";
-            navArea.style.background = "none";
+            navArea.style.transform =  "translateX(-15em)";
         });
     }
     else {
-        // main.style.opacity = "1";
-        navContent.style.display = "block";
-        navArea.style.background = "none";
-
+        navArea.className = "";
+        navArea.style.transform = "translateX(0)";
         $(".nav-bar").unbind();
         $("#nav").unbind();
     }
